9fbeae1ba4f0fba0444151e2d260517f81629e74
Nathan Whyte <nathanwhyte35@gmail.com>
2021-09-27 21:47:47 -0500

    Build Markdown file metadata struct (fd and title)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{fs::File, process::exit};
+use std::{
+	fs::File,
+	io::{BufRead, BufReader},
+	process::exit,
+};
 
 /// File actions (open, create, read lines, etc.)
 mod files;
 
-enum SyntaxElementType {
-	Header,
-	UnorderedEntry,
-	OrderedEntry,
+struct MarkdownFile {
+	markdown_file_fd: File,
+	title: String,
 }
 
 struct MarkdownSyntaxElement {
@@ -14,6 +17,12 @@ struct MarkdownSyntaxElement {
 	element_type: SyntaxElementType,
 	list_order: i32,
 	element_text: String,
+}
+
+enum SyntaxElementType {
+	Header,
+	UnorderedEntry,
+	OrderedEntry,
 }
 
 /// Indent by two spaces for sub-items
@@ -25,9 +34,10 @@ static MAX_DEPTH: i32 = 5;
 fn main() {
 	let args: Vec<String> = collect_args();
 
-	let markdown_file: File = files::open_file(&args[1].clone());
+	let markdown_file: MarkdownFile = get_markdown_file(args[1].clone());
 }
 
+/// Collects command line arguments and handles an incorrect amount of arguments
 fn collect_args() -> Vec<String> {
 	let args: Vec<String> = std::env::args().collect();
 
@@ -37,4 +47,30 @@ fn collect_args() -> Vec<String> {
 	}
 
 	return args;
+}
+
+/// Open the Markdown file at given path and build MarkdownFile struct for that file
+fn get_markdown_file(markdown_file_path: String) -> MarkdownFile {
+	let markdown_file: File = files::open_file(&markdown_file_path);
+	let markdown_file_title: String = get_markdown_file_title(&markdown_file);
+
+	MarkdownFile {
+		markdown_file_fd: markdown_file,
+		title: markdown_file_title,
+	}
+}
+
+/// Get the title of a Markdown file (text from the first level 1 heading)
+fn get_markdown_file_title(markdown_file: &File) -> String {
+	let read_buffer = BufReader::new(markdown_file);
+	let mut title_line: String = String::new();
+
+	for line in read_buffer.lines().flatten() {
+		if line.contains("#") {
+			title_line = line.clone();
+			break;
+		}
+	}
+
+	return title_line.trim().replacen("# ", "", 1);
 }
