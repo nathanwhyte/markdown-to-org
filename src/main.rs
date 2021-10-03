368946d1e26aeeb2af80ddfb474caddd2a133485
Nathan Whyte <nathanwhyte35@gmail.com>
2021-10-02 21:28:40 -0500

    Convert headers, list items from Markdown to Org

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::Write;
 use std::process::exit;
 
 /// File actions (open, create, read lines, etc.)
@@ -6,7 +7,7 @@ mod files;
 
 struct MarkdownFile {
 	fd: File,
-	file_name: String,
+	file_path: String,
 	title: String,
 }
 
@@ -19,7 +20,7 @@ enum SyntaxElementType {
 struct MarkdownSyntaxElement {
 	element_depth: i32,
 	element_type: SyntaxElementType,
-	list_order: i32,
+	list_order: char,
 	element_text: String,
 }
 
@@ -33,7 +34,7 @@ fn main() {
 	let args: Vec<String> = collect_args();
 	let markdown_file_struct: MarkdownFile = get_markdown_file(args[1].clone());
 
-	convert_to_org(markdown_file_struct);
+	convert_to_org(markdown_file_struct, args[1].clone());
 }
 
 /// Collects command line arguments and handles an incorrect amount of arguments
@@ -52,12 +53,11 @@ fn collect_args() -> Vec<String> {
 fn get_markdown_file(markdown_file_path: String) -> MarkdownFile {
 	let markdown_file: File = files::open_file(&markdown_file_path);
 	let markdown_file_title: String = get_markdown_file_title(markdown_file_path.clone());
-	let markdown_file_name: String = get_markdown_file_name(markdown_file_path.clone());
 
 	return MarkdownFile {
 		fd: markdown_file,
 		title: markdown_file_title,
-		file_name: markdown_file_name,
+		file_path: markdown_file_path,
 	};
 }
 
@@ -76,27 +76,15 @@ fn get_markdown_file_title(markdown_file_path: String) -> String {
 	return title_line.trim().replacen("# ", "", 1);
 }
 
-/// Get the name of the given Markdown file (name + extension)
-fn get_markdown_file_name(mut markdown_file_path: String) -> String {
-	let mut markdown_file_name: String = String::new();
-
-	if !markdown_file_path.contains("/") {
-		return markdown_file_path;
-	}
-
-	let mut current_char: char = markdown_file_path.pop().unwrap();
-	while current_char != '/' {
-		markdown_file_name.insert(0, current_char);
-		current_char = markdown_file_path.pop().unwrap();
-	}
-
-	return markdown_file_name;
-}
-
 /// Driver function for converting Markdown to Org
-fn convert_to_org(markdown_file_struct: MarkdownFile) {
-	// TODO read lines and build struct for each one
-	let mut syntax_elements: Vec<MarkdownSyntaxElement> = get_syntax_elements(markdown_file_struct);
+fn convert_to_org(markdown_file_struct: MarkdownFile, path: String) {
+	let markdown_file_path = path.clone().replace(".md", ".org");
+	let org_file: File = files::create_file(&markdown_file_path);
+	let file_title: String = markdown_file_struct.title.clone();
+
+	let syntax_elements_vec: Vec<MarkdownSyntaxElement> = get_syntax_elements(markdown_file_struct);
+
+	write_to_org_file(org_file, file_title, syntax_elements_vec);
 }
 
 /// Build a vector of Markdown syntax elements for each valid line in the given Markdown file
@@ -160,18 +148,62 @@ fn get_syntax_element_type(line: String) -> SyntaxElementType {
 	return syntax_element_type;
 }
 
-fn get_list_order(element_type: &SyntaxElementType, line: String) -> i32 {
+/// Get the list order of a ordered list item
+fn get_list_order(element_type: &SyntaxElementType, line: String) -> char {
 	return match element_type {
-		SyntaxElementType::Header => 0,
-		SyntaxElementType::UnorderedEntry => 0,
-		SyntaxElementType::OrderedEntry => line.chars().nth(0).unwrap().to_digit(10).unwrap() as i32,
+		SyntaxElementType::Header => '*',
+		SyntaxElementType::UnorderedEntry => '-',
+		SyntaxElementType::OrderedEntry => line.chars().nth(0).unwrap(),
 	};
 }
 
+/// Get the text of a Markdown syntax element
 fn get_element_text(mut line: String) -> String {
 	while !line.chars().next().unwrap().is_alphabetic() {
 		line.remove(0);
 	}
 
 	return line.clone().trim().to_string();
+}
+
+/// Driver function for converting Markdown syntax to the Org
+fn write_to_org_file(mut org_file: File, title: String, elements: Vec<MarkdownSyntaxElement>) {
+	writeln!(&mut org_file, "#+TITLE: {}", title).expect("Error writing title line");
+
+	for element in elements {
+		match element.element_type {
+			SyntaxElementType::Header => write_header(&org_file, element),
+			SyntaxElementType::UnorderedEntry => write_list_element(&org_file, element, false),
+			SyntaxElementType::OrderedEntry => write_list_element(&org_file, element, true),
+		}
+	}
+}
+
+fn write_header(mut org_file: &File, element: MarkdownSyntaxElement) {
+	writeln!(&mut org_file, "").expect("Error writing blank line");
+
+	for _star in 0..element.element_depth {
+		write!(&mut org_file, "*").expect("Error writing header");
+	}
+
+	write!(&mut org_file, " {}\n", element.element_text).expect("Error writing header");
+}
+
+fn write_list_element(mut org_file: &File, element: MarkdownSyntaxElement, is_ordered: bool) {
+	if is_ordered {
+		writeln!(
+			&mut org_file,
+			"{}. {}",
+			element.list_order, element.element_text
+		)
+		.expect("Error writing list element");
+	} else {
+		writeln!(
+			&mut org_file,
+			"{} {}",
+			element.list_order, element.element_text
+		)
+		.expect("Error writing list element");
+
+	}
 }
